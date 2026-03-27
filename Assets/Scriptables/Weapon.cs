5d25564c0d515c4d769f5aad0b47a8bc2ebacad4
LetuCoding <strikerletu@gmail.com>
2026-03-27 20:45:21 +0100

    Variety of weapons and ammo added

--- Assets/Scriptables/Weapon.cs
+++ Assets/Scriptables/Weapon.cs
@@ -2,28 +2,33 @@ using UnityEngine;
 
 namespace Scriptables
 {
+    public enum FireType
+    {
+        SemiAuto,
+        FullAuto,
+        Burst,
+        Shotgun
+    }
+
     [CreateAssetMenu(fileName = "Weapon", menuName = "Scriptable Objects/Weapon")]
     public class Weapon : ScriptableObject
     {
         [SerializeField] private string model;
-        
         [SerializeField] private int damage;
-        
         [SerializeField] private float fireRate;
-
         [SerializeField] private float firePower;
-        
         [SerializeField] private float reloadTime;
-        
         [SerializeField] private int maxAmmo;
-        
         [SerializeField] private int magCapacity;
-        
         [SerializeField] private Bullet bullet;
-    
         [SerializeField] private AudioClip fireSound;
-        
-        //getters setters
+
+        [Header("Fire Mode")]
+        [SerializeField] private FireType fireType;
+        [SerializeField] private int burstCount = 3;
+        [SerializeField] private int pelletsPerShot = 8;
+        [SerializeField, Range(0f, 1f)] private float spread = 0.08f;
+
         public string Model { get => model; set => model = value; }
         public int Damage { get => damage; set => damage = value; }
         public float FireRate { get => fireRate; set => fireRate = value; }
@@ -33,5 +38,10 @@ namespace Scriptables
         public float FirePower { get => firePower; set => firePower = value; }
         public Bullet Bullet { get => bullet; set => bullet = value; }
         public AudioClip FireSound { get => fireSound; set => fireSound = value; }
+
+        public FireType FireType { get => fireType; set => fireType = value; }
+        public int BurstCount { get => burstCount; set => burstCount = value; }
+        public int PelletsPerShot { get => pelletsPerShot; set => pelletsPerShot = value; }
+        public float Spread { get => spread; set => spread = value; }
     }
 }
--- Assets/WeaponScript.cs
+++ Assets/WeaponScript.cs
@@ -7,126 +7,215 @@ using UnityEngine.XR.Interaction.Toolkit.Interactables;
 public class WeaponScript : MonoBehaviour
 {
     private XRGrabInteractable _interactable;
-    
+
     [SerializeField] private Weapon weapon;
-    
+
     [Header("Audio Settings")]
-    public AudioSource audioSource;
-    private AudioClip shoot; 
-    
+    [SerializeField] private AudioSource audioSource;
+
     [Header("Fire Settings")]
-    public Transform firePoint; 
-    private float fireRate;   
+    [SerializeField] private Transform firePoint;
+
+    [Header("VFX Settings")]
+    [SerializeField] private ParticleSystem muzzleFlash;
+
+    private AudioClip shoot;
+    private float fireRate;
     private float firePower;
     private int ammo;
-    
-    [Header("VFX Settings")]
-    public ParticleSystem muzzleFlash; 
-    
-    private float nextFireTime = 0f;
-    private bool isFiring = false;
-    private bool isReloading = false;
 
-    void Start()
-    {
-        muzzleFlash.Stop();
-       
-        if (weapon != null)
-        {
-            shoot = weapon.FireSound;
-            fireRate = weapon.FireRate;
-            firePower = weapon.FirePower;
-            ammo = weapon.MagCapacity;
-        }
-        
-    }
-    void Awake()
+    private float nextFireTime;
+    private bool isFiring;
+    private bool isReloading;
+    private bool burstInProgress;
+
+    private void Awake()
     {
         _interactable = GetComponent<XRGrabInteractable>();
     }
 
-    void OnEnable()
+    private void Start()
+    {
+        if (weapon == null) return;
+
+        shoot = weapon.FireSound;
+        fireRate = weapon.FireRate;
+        firePower = weapon.FirePower;
+        ammo = weapon.MagCapacity;
+
+        if (muzzleFlash != null)
+            muzzleFlash.Stop();
+    }
+
+    private void OnEnable()
     {
         _interactable.activated.AddListener(StartFiring);
         _interactable.deactivated.AddListener(StopFiring);
     }
 
-    void OnDisable()
+    private void OnDisable()
     {
         _interactable.activated.RemoveListener(StartFiring);
         _interactable.deactivated.RemoveListener(StopFiring);
     }
 
-    void Update()
+    private void Update()
     {
-        if (!isFiring || isReloading) return;
+        if (isReloading || weapon == null) return;
+
+        switch (weapon.FireType)
+        {
+            case FireType.FullAuto:
+                HandleFullAuto();
+                break;
+
+            case FireType.Burst:
+                HandleBurst();
+                break;
+        }
+    }
+
+    private void StartFiring(ActivateEventArgs args)
+    {
+        if (weapon == null || isReloading) return;
+
+        switch (weapon.FireType)
+        {
+            case FireType.SemiAuto:
+                TryShoot();
+                break;
+
+            case FireType.FullAuto:
+                isFiring = true;
+                break;
+
+            case FireType.Burst:
+                TryStartBurst();
+                break;
+
+            case FireType.Shotgun:
+                TryShoot();
+                break;
+        }
+    }
+
+    private void StopFiring(DeactivateEventArgs args)
+    {
+        isFiring = false;
+    }
+
+    private void HandleFullAuto()
+    {
+        if (!isFiring) return;
 
         if (Time.time >= nextFireTime)
         {
-            Shoot();
+            TryShoot();
             nextFireTime = Time.time + fireRate;
         }
     }
 
-    void StartFiring(ActivateEventArgs args)
+    private void HandleBurst()
     {
-        
-        isFiring = true;
+        if (!isFiring) return;
     }
 
-    /*void StartFiring2()
+    private void TryStartBurst()
     {
-        
-        isFiring = true;
-    }*/
-    
-    
-    void StopFiring(DeactivateEventArgs args)
-    {
-        isFiring = false;
-        //muzzleFlash.Stop();
+        if (burstInProgress) return;
+        StartCoroutine(BurstRoutine());
     }
 
-    void Shoot()
+    private IEnumerator BurstRoutine()
     {
-        if (ammo > 0)
+        burstInProgress = true;
+        isFiring = true;
+
+        for (int i = 0; i < weapon.BurstCount; i++)
         {
-            ammo--;
-            audioSource.PlayOneShot(shoot);
+            if (isReloading || ammo <= 0) break;
 
-            if (muzzleFlash != null)
-            {
-                muzzleFlash.Emit(1); // emite 1 partícula instantánea
-            }
-
-            GameObject bulletGO = Instantiate(weapon.Bullet.BulletPrefab, firePoint.position, firePoint.rotation);
-            BulletScript bulletScript = bulletGO.GetComponent<BulletScript>();
-
-            
-            bulletScript.Init(weapon.Bullet, 2.5f);
-
-           
-            bulletScript.Launch(firePoint.forward, firePower);
-            
+            TryShoot();
+            yield return new WaitForSeconds(fireRate);
         }
-        else
+
+        isFiring = false;
+        burstInProgress = false;
+    }
+
+    private void TryShoot()
+    {
+        if (ammo <= 0)
         {
             if (!isReloading)
                 StartCoroutine(Reload());
+            return;
         }
-        
+
+        ammo--;
+
+        if (audioSource != null && shoot != null)
+            audioSource.PlayOneShot(shoot);
+
+        if (muzzleFlash != null)
+            muzzleFlash.Emit(1);
+
+        switch (weapon.FireType)
+        {
+            case FireType.Shotgun:
+                FireShotgun();
+                break;
+
+            default:
+                FireSingleBullet(firePoint.forward, firePoint.rotation);
+                break;
+        }
+
+        if (ammo <= 0 && !isReloading)
+            StartCoroutine(Reload());
+    }
+
+    private void FireSingleBullet(Vector3 direction, Quaternion rotation)
+    {
+        GameObject bulletGO = Instantiate(weapon.Bullet.BulletPrefab, firePoint.position, rotation);
+        BulletScript bulletScript = bulletGO.GetComponent<BulletScript>();
+
+        bulletScript.Init(weapon.Bullet, 2.5f);
+        bulletScript.Launch(direction.normalized, firePower);
+    }
+
+    private void FireShotgun()
+    {
+        int pellets = Mathf.Max(1, weapon.PelletsPerShot);
+
+        for (int i = 0; i < pellets; i++)
+        {
+            Vector3 spreadDir = GetSpreadDirection(firePoint.forward, weapon.Spread);
+            Quaternion rot = Quaternion.LookRotation(spreadDir);
+
+            FireSingleBullet(spreadDir, rot);
+        }
+    }
+
+    private Vector3 GetSpreadDirection(Vector3 forward, float spread)
+    {
+        Vector3 randomSpread = new Vector3(
+            Random.Range(-spread, spread),
+            Random.Range(-spread, spread),
+            Random.Range(-spread, spread)
+        );
+
+        return (forward + randomSpread).normalized;
     }
 
     private IEnumerator Reload()
     {
         isReloading = true;
-
-        Debug.Log("Reload called");
+        isFiring = false;
 
         yield return new WaitForSeconds(weapon.ReloadTime);
 
         ammo = weapon.MagCapacity;
-
         isReloading = false;
     }
 }
